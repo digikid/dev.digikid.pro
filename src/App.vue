591f1ca5692f05327802578990814379bdec2f2e
digikid <dev@digikid.ru>
2022-10-27 04:24:55 +0500

    Update: Show greetings message

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import {
-  ref, watch, onMounted, markRaw, inject,
+  ref, computed, watch, onMounted, markRaw, inject,
 } from 'vue';
 
 import { useGlobalStore } from '@stores/global';
@@ -12,6 +12,9 @@ import { useRouting } from '@hooks/routing';
 import { set as setLocal } from '@utils/localStorage';
 import { updateTitle } from '@utils/dom';
 import { capitalize } from '@utils/string';
+import { print } from '@utils/log';
+
+import config from '@src/config';
 
 import MainLayout from '@layouts/MainLayout.vue';
 
@@ -25,11 +28,29 @@ const layout = ref();
 
 const metrika = inject<YandexMetrika.Counter>('metrika');
 
-if (store.colorMode === 'dark') {
-  document.documentElement.classList.add('dark');
-}
+const greetings = computed(() => {
+  const main = messages.value?.main as any;
+
+  return (main.console ? main.console : []) as string[];
+});
+
+const showGreetings = (clear = false) => {
+  if (config.prod) {
+    if (clear) {
+      console.clear();
+    }
+
+    print(greetings.value, true);
+  }
+};
 
 (async () => {
+  if (store.colorMode === 'dark') {
+    document.documentElement.classList.add('dark');
+  }
+
+  showGreetings();
+
   await dataStore.loadVersions();
 })();
 
@@ -66,6 +87,8 @@ watch(() => locale.value, (value, prev) => {
   document.documentElement.lang = value;
 
   setLocal('user-locale', value);
+
+  showGreetings(true);
 
   if (prev && (prev !== value)) {
     const page = (messages.value.pages as LocaleRecord[]).find((item) => item.id === route.name);
